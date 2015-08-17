ab06d1f130ba92e4ea410e8c57634d90f013fe21
eliasbrewer <eliasbrewer@gmail.com>
2015-08-16 20:10:02 -0600

    Test
    Test

--- cachematrix.R
+++ cachematrix.R
@@ -8,8 +8,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## 
 }
